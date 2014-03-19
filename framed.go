d9d39f5a507abb4abc8b5b90c3744e30b4507bcb
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-03-18 22:55:45 -0500

    Redid API to support zero-copy ops

--- framed.go
+++ framed.go
@@ -1,11 +1,16 @@
 /*
 Package framed adds basic support for message framing over streams.
 
-Message are length-prefixed.  The first two bytes are an unsigned 16 bit int
-stored in little-endian byte order.  The remaining bytes are the bytes of the
-message.
+Messages contain an header and a body, both of which are length prefixed.
 
-The use of a uint16 means that the maximum possible data length is 65535.
+Here are the bytes (stored in little-endian byte order):
+
+0-2: unsigned 16 bit int header length
+2-4: unsinged 16 bit int body length
+4+:  message content (header and body)
+
+The use of a uint16 means that the maximum possible header and body lengths
+are 65535 each.
 
 Example:
 
@@ -33,7 +38,9 @@ package framed
 
 import (
 	"encoding/binary"
+	"fmt"
 	"io"
+	"io/ioutil"
 )
 
 var endianness = binary.LittleEndian
@@ -47,43 +54,150 @@ goroutines, a Framed is not.
 */
 type Framed struct {
 	io.ReadWriteCloser // the raw underlying connection
+	hasReadInitial     bool
+}
+
+type AlreadyReadError string
+
+func (err AlreadyReadError) Error() string {
+	return string(err)
+}
+
+type BufferTooSmall string
+
+func (err BufferTooSmall) Error() string {
+	return string(err)
+}
+
+type Frame struct {
+	framed        *Framed
+	headerLength  int16
+	bodyLength    int16
+	hasReadHeader bool
+	hasReadBody   bool
+}
+
+func NewFramed(readWriteCloser io.ReadWriteCloser) *Framed {
+	return &Framed{readWriteCloser, false}
 }
 
 /*
-ReadFrame reads the next frame from the Framed.
+ReadInitial reads the initial frame from the framed
 */
-func (framed Framed) ReadFrame() (frame []byte, err error) {
-	var nb uint16
-	err = binary.Read(framed, endianness, &nb)
-	if err != nil {
-		return
+func (framed *Framed) ReadInitial() (frame *Frame, err error) {
+	if framed.hasReadInitial {
+		return nil, AlreadyReadError("Initial Frame already read")
 	}
-	numBytes := int(nb)
-	frame = make([]byte, numBytes)
-	for totalRead := 0; totalRead < numBytes; {
-		var bytesRead int
-		bytesRead, err = framed.Read(frame[totalRead:])
-		if err != nil {
-			return
-		}
-		totalRead += bytesRead
-	}
+	frame, err = framed.nextFrame()
+	framed.hasReadInitial = true
 	return
 }
 
-/*
-WriteFrame writes all of the supplied bytes to the Framed as a single frame.
-*/
-func (framed Framed) WriteFrame(byteArrays ...[]byte) (err error) {
-	var numBytes uint16
-	for _, bytes := range byteArrays {
-		numBytes += uint16(len(bytes))
+func (frame *Frame) NextFrame() (nextFrame *Frame, err error) {
+	if !frame.hasReadBody {
+		frame.CopyBody(ioutil.Discard)
 	}
-	err = binary.Write(framed, endianness, numBytes)
-	for _, bytes := range byteArrays {
-		if _, err = framed.Write(bytes); err != nil {
-			return
+	nextFrame, err = frame.framed.nextFrame()
+	return
+}
+
+func (frame *Frame) CopyHeader(out io.Writer) (err error) {
+	if frame.hasReadHeader {
+		return AlreadyReadError("Header Already Read")
+	}
+	_, err = io.CopyN(out, frame.framed, int64(frame.headerLength))
+	frame.hasReadHeader = true
+	return
+}
+
+func (frame *Frame) CopyBody(out io.Writer) (err error) {
+	if !frame.hasReadHeader {
+		if err := frame.CopyHeader(ioutil.Discard); err != nil {
+			return err
 		}
 	}
+	if frame.hasReadBody {
+		err = AlreadyReadError("Body Already Read")
+		return
+	}
+	_, err = io.CopyN(out, frame.framed, int64(frame.bodyLength))
+	frame.hasReadBody = true
+	return
+}
+
+func (frame *Frame) ReadHeader(buffer []byte) (n int, err error) {
+	if len(buffer) < int(frame.headerLength) {
+		return 0, BufferTooSmall(fmt.Sprintf("Buffer too small. %d bytes required for header.", frame.headerLength))
+	}
+	if frame.hasReadHeader {
+		return 0, AlreadyReadError("Header Already Read")
+	}
+	frame.framed.Read(buffer[0:frame.headerLength])
+	frame.hasReadHeader = true
+	return
+}
+
+func (frame *Frame) ReadBody(buffer []byte) (n int, err error) {
+	if len(buffer) < int(frame.bodyLength) {
+		return 0, BufferTooSmall(fmt.Sprintf("Buffer too small. %d bytes required for body.", frame.bodyLength))
+	}
+	if !frame.hasReadHeader {
+		if err := frame.CopyHeader(ioutil.Discard); err != nil {
+			return 0, err
+		}
+	}
+	if frame.hasReadBody {
+		return 0, AlreadyReadError("Body Already Read")
+	}
+	frame.framed.Read(buffer[0:frame.bodyLength])
+	frame.hasReadBody = true
+	return
+}
+
+func (framed *Framed) WriteFrame(header []byte, body []byte) (err error) {
+	if err = framed.WriteHeader(int16(len(header)), int16(len(body))); err != nil {
+		return err
+	}
+	if _, err = framed.Write(header); err != nil {
+		return err
+	}
+	_, err = framed.Write(body)
+	return
+}
+
+func (framed *Framed) WriteHeader(headerLength int16, bodyLength int16) (err error) {
+	return writeHeaderTo(framed, headerLength, bodyLength)
+}
+
+func (frame *Frame) CopyTo(out io.Writer) (err error) {
+	if err = writeHeaderTo(out, frame.headerLength, frame.bodyLength); err != nil {
+		return
+	}
+	if err = frame.CopyHeader(out); err != nil {
+		return
+	}
+	err = frame.CopyBody(out)
+	return
+}
+
+func (frame *Frame) readLengths() (err error) {
+	if err = binary.Read(frame.framed, endianness, &frame.headerLength); err != nil {
+		return
+	}
+	err = binary.Read(frame.framed, endianness, &frame.bodyLength)
+	return
+}
+
+func writeHeaderTo(out io.Writer, headerLength int16, bodyLength int16) (err error) {
+	if err = binary.Write(out, endianness, headerLength); err != nil {
+		return
+	}
+	err = binary.Write(out, endianness, bodyLength)
+	return
+}
+
+func (framed *Framed) nextFrame() (frame *Frame, err error) {
+	frame = &Frame{framed: framed}
+	err = frame.readLengths()
 	return
 }
--- framed_test.go
+++ framed_test.go
@@ -21,20 +21,77 @@ func (buffer CloseableBuffer) Close() (err error) {
 	return
 }
 
-func TestFraming(t *testing.T) {
-	msgPart1 := []byte("This is a ")
-	msgPart2 := []byte("test message")
-	testMessage := []byte("This is a test message")
+func TestReadToBytes(t *testing.T) {
+	header := []byte("Header Header header Header")
+	body := []byte("Body Body Body Body Body Body")
 	buffer := CloseableBuffer{bytes.NewBuffer(make([]byte, 0))}
-	fbuffer := Framed{buffer}
-	if err := fbuffer.WriteFrame(msgPart1, msgPart2); err != nil {
-		t.Errorf("Unable to write: %s", err)
+	fbuffer := NewFramed(buffer)
+	if err := fbuffer.WriteFrame(header, body); err != nil {
+		t.Fatalf("Unable to write: %s", err)
 	}
-	if receivedMsg, err := fbuffer.ReadFrame(); err != nil {
-		t.Errorf("Unable to read: %s", err)
-	} else {
-		if !bytes.Equal(receivedMsg, testMessage) {
-			t.Errorf("Received did not match expected.  Expected: '%s', Received: '%s'", string(testMessage), string(receivedMsg))
-		}
+	frame, err := fbuffer.ReadInitial()
+	if err != nil {
+		t.Fatalf("Unable to read frame: %s", err)
+	}
+	readHeader := make([]byte, len(header))
+	readBody := make([]byte, len(body))
+	if int(frame.headerLength) != len(header) {
+		t.Errorf("Expected headerLength %d, got %d", len(header), frame.headerLength)
+	}
+	if int(frame.bodyLength) != len(body) {
+		t.Errorf("Expected headerLength %d, got %d", len(body), frame.bodyLength)
+	}
+	if _, err = frame.ReadHeader(readHeader); err != nil {
+		t.Fatalf("Unable to read header: %s", err)
+	}
+	if _, err = frame.ReadBody(readBody); err != nil {
+		t.Fatalf("Unable to read body: %s", err)
+	}
+	if !bytes.Equal(readHeader, header) {
+		t.Errorf("Header did not match expected.  Expected: '%s', Received: '%s'", string(header), string(readHeader))
+	}
+	if !bytes.Equal(readBody, body) {
+		t.Errorf("Body did not match expected.  Expected: '%s', Received: '%s'", string(body), string(readBody))
+	}
+}
+
+func TestReadToStreamWithCopy(t *testing.T) {
+	header := []byte("Header Header header Header")
+	body := []byte("Body Body Body Body Body Body")
+	buffer := CloseableBuffer{bytes.NewBuffer(make([]byte, 0))}
+	fbuffer := NewFramed(buffer)
+	buffer2 := CloseableBuffer{bytes.NewBuffer(make([]byte, 0))}
+	fbuffer2 := NewFramed(buffer2)
+	if err := fbuffer.WriteFrame(header, body); err != nil {
+		t.Fatalf("Unable to write: %s", err)
+	}
+	frame, err := fbuffer.ReadInitial()
+	if err != nil {
+		t.Fatalf("Unable to read frame: %s", err)
+	}
+	frame.CopyTo(buffer2)
+	frame, err = fbuffer2.ReadInitial()
+	if err != nil {
+		t.Fatalf("Unable to read frame: %s", err)
+	}
+	readHeader := bytes.NewBuffer(make([]byte, 0))
+	readBody := bytes.NewBuffer(make([]byte, 0))
+	if int(frame.headerLength) != len(header) {
+		t.Errorf("Expected headerLength %d, got %d", len(header), frame.headerLength)
+	}
+	if int(frame.bodyLength) != len(body) {
+		t.Errorf("Expected headerLength %d, got %d", len(body), frame.bodyLength)
+	}
+	if err = frame.CopyHeader(readHeader); err != nil {
+		t.Fatalf("Unable to read header: %s", err)
+	}
+	if err = frame.CopyBody(readBody); err != nil {
+		t.Fatalf("Unable to read body: %s", err)
+	}
+	if !bytes.Equal(readHeader.Bytes(), header) {
+		t.Errorf("Header did not match expected.  Expected: '%s', Received: '%s'", string(header), string(readHeader.Bytes()))
+	}
+	if !bytes.Equal(readBody.Bytes(), body) {
+		t.Errorf("Body did not match expected.  Expected: '%s', Received: '%s'", string(body), string(readBody.Bytes()))
 	}
 }
